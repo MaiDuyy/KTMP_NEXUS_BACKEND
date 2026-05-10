62dca601f325e2384404b11e760103d65fa4c11f
MaiDuyy <phammainhat123@gmail.com>
2026-05-10 23:33:59 +0700

    chore: remove repo-chat-test-be-master from the repository

--- repo-chat-test-be-master/add-env-grpc.ps1
+++
@@ -1,9 +0,0 @@
-
-"
-# ======================
-# gRPC Configuration
-# ======================
-IDENTITY_GRPC_PORT=50051
-IDENTITY_GRPC_HOST=localhost:50051
-" | Out-File -FilePath .env -Append -Encoding utf8
-
